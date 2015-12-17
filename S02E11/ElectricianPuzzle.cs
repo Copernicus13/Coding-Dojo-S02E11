c4e42bb84e1e26cc1fb9ff2000c702b83f4f81fe
Michel <copernicus_13@hotmail.com>
2015-12-17 12:08:31 +0100

    Commit of the puzzle.

---
+++ S02E11/ElectricianPuzzle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingDojo.S02E11
+{
+    public class ElectricianPuzzle
+    {
+        public void ParseInputFromConsole()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ParseInput(IEnumerable<Tuple<int, int>> data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ComputeSolution()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- S02E11/Program.cs
+++ S02E11/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace CodingDojo.S02E11
+﻿using System;
+
+namespace CodingDojo.S02E11
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var solver = new ElectricianPuzzle();
+            solver.ParseInputFromConsole();
+            var result = solver.ComputeSolution();
+            Console.WriteLine("Le résultat est : {0}", result);
+
+            Console.Write("Appuyez sur une touche pour continuer…");
+            Console.ReadKey();
         }
     }
 }
